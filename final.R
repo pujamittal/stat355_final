99cf34ede382890deaeb260f1c3b1103a0f9e0e9
pujamittal <mittal21@purdue.edu>
2018-04-19 02:43:43 -0400

    made the data to look at for each painter and painter by school, need to do t-tests still

--- final.R
+++ final.R
@@ -1,1 +1,12 @@
 #this is the base file
+library(MASS)
+attach(painters)
+
+#this sums each painter
+painter_sums <- data.frame(rowSums(painters[,1:4]))
+colnames(painter_sums) <- c('sum')
+
+#sum of scores by each school
+aggregate(. ~ School, painters, sum)
+
+# need to do a t-test
