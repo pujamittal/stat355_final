33f32c55bb5b25de0e833b7d138795d55f572a28
Keerthiha <keerthi@pal-nat186-11-217.itap.purdue.edu>
2018-04-23 19:29:58 -0400

    adding in remaining tests minus one

--- final.R
+++ final.R
@@ -96,3 +96,123 @@ t.test(groupH_exp, alternative = "greater", mu = 10)
 #yes
 #p-value < 2.2e-16
 #mean 12.04
+
+#t-Test to compare the composition scores of B to the 16th century category 
+
+group1<-subset(painters$Composition, (painters$School == "B"))
+groupBE=sample(group1,1000,replace = TRUE)
+group2<-subset(painters$Composition, (painters$School == "F"))
+groupF=sample(group2,1000,replace = TRUE)
+
+t.test(groupBE,groupF)
+
+#t-Test to compare the drawing scores of B to the 16th century category 
+
+groupd1<-subset(painters$Drawing, (painters$School == "B" ))
+groupD1<-sample(groupd1,1000,replace = TRUE)
+groupd2<-subset(painters$Drawing, (painters$School == "F"))
+groupD2=sample(groupd2,1000,replace = TRUE)
+
+t.test(groupD1,groupD2)
+
+#t-Test to compare the Colour scores of B to the 16th century category 
+
+groupc1<-subset(painters$Colour, (painters$School == "B"))
+groupC1<-sample(groupc1,1000,replace = TRUE)
+groupc2<-subset(painters$Colour, (painters$School == "F"))
+groupC2=sample(groupc2,1000,replace = TRUE)
+
+t.test(groupC1,groupC2)
+
+#t-Test to compare the Expression scores of B to the 16th century category 
+
+groupe1<-subset(painters$Expression, (painters$School == "B"))
+plotgroupE1<-sample(groupe1,1000,replace = TRUE)
+groupe2<-subset(painters$Expression, (painters$School == "F"))
+groupE2=sample(groupe2,1000,replace = TRUE)
+
+t.test(groupE1,groupE2)
+
+#t-Test to compare sum of all the scores of B to the 16th century category 
+
+groupsum1<-subset((Composition+Drawing+Colour+Expression), (painters$School == "B"))
+groupS1<-sample(groupsum1,1000,replace=TRUE)
+groupsum2<-subset((Composition+Drawing+Colour+Expression),(painters$School == "F"))
+groupS2<-sample(groupsum2,1000,replace = TRUE)
+
+t.test(groupS1,groupS2)
+
+#t-Test to compare the composition scores of C to the 17th century category 
+
+group1<-subset(painters$Composition, (painters$School == "C"))
+groupBE=sample(group1,1000,replace = TRUE)
+group2<-subset(painters$Composition, (painters$School == "G"))
+groupF=sample(group2,1000,replace = TRUE)
+
+t.test(groupBE,groupF)
+
+#t-Test to compare the drawing scores of C to the 17th century category 
+
+groupd1<-subset(painters$Drawing, (painters$School == "C" ))
+groupD1<-sample(groupd1,1000,replace = TRUE)
+groupd2<-subset(painters$Drawing, (painters$School == "G"))
+groupD2=sample(groupd2,1000,replace = TRUE)
+
+t.test(groupD1,groupD2)
+
+#t-Test to compare the Colour scores of C to the 17th century category 
+
+groupc1<-subset(painters$Colour, (painters$School == "C"))
+groupC1<-sample(groupc1,1000,replace = TRUE)
+groupc2<-subset(painters$Colour, (painters$School == "G"))
+groupC2=sample(groupc2,1000,replace = TRUE)
+
+t.test(groupC1,groupC2)
+
+#t-Test to compare the Expression scores of C to the 17th century category 
+
+groupe1<-subset(painters$Expression, (painters$School == "C"))
+plotgroupE1<-sample(groupe1,1000,replace = TRUE)
+groupe2<-subset(painters$Expression, (painters$School == "G"))
+groupE2=sample(groupe2,1000,replace = TRUE)
+
+t.test(groupE1,groupE2)
+
+#t-Test to compare sum of all the scores of C to the 17th century category 
+
+groupsum1<-subset((Composition+Drawing+Colour+Expression), (painters$School == "C"))
+groupS1<-sample(groupsum1,1000,replace=TRUE)
+groupsum2<-subset((Composition+Drawing+Colour+Expression),(painters$School == "G"))
+groupS2<-sample(groupsum2,1000,replace = TRUE)
+
+t.test(groupS1,groupS2)
+
+#Anova(used to compare the three periods A,C,D to see if the time periods have any effect on the scores)
+
+#Sum of all the scores 
+myanova<-aov((Composition+Drawing+Colour+Expression)~School , data=groupACD)
+summary(myanova)
+
+#Composition scores
+myanova<-aov((Composition)~School , data=groupACD)
+summary(myanova)
+
+#Drawing scores
+myanova<-aov((Drawing)~School , data=groupACD)
+summary(myanova)
+
+#Colour scores
+myanova<-aov((Colour)~School , data=groupACD)
+summary(myanova)
+
+#Expression scores
+myanova<-aov((Expression)~School , data=groupACD)
+summary(myanova)
+
+#Boxplots
+
+boxplot(Composition~School , ylab = " Composition Scores" , xlab="The school")
+boxplot(Drawing~School , ylab = " Drawing Scores" , xlab="The school")
+boxplot(Colour~School , ylab = " Colour Scores" , xlab="The school")
+boxplot(Expression~School , ylab = " Expression Scores" , xlab="The school") 
+boxplot((Composition+Drawing+Colour+Expression)~School , ylab = "Sum of all the Scores" , xlab="The school")
